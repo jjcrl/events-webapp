f55f0708bce13361e0f06b00729b3b7825615d5b
Zein Rafie <zeinrafie1@gmail.com>
2026-06-22 17:01:58 +0100

    removed acebook frontend, set up fresh vite react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import "./App.css";
-import { HomePage } from "./pages/Home/HomePage";
-import { LoginPage } from "./pages/Login/LoginPage";
-import { SignupPage } from "./pages/Signup/SignupPage";
-import { FeedPage } from "./pages/Feed/FeedPage";
-
-// docs: https://reactrouter.com/en/main/start/overview
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/signup",
-    element: <SignupPage />,
-  },
-  {
-    path: "/posts",
-    element: <FeedPage />,
-  },
-]);
-
-function App() {
+const App = () => {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+    <div>
+      <h1>Events App</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,10 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import App from "./App.jsx";
-import "./index.css";
-
-// Get the "root" div from index.html.
-// The React application will be inserted into this div.
-const rootElement = document.getElementById("root");
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
