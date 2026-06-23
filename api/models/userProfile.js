826eb69077fbbb15f36a4751f89fd81e99f2ea82
jjcrl <j412cff@icloud.com>
2026-06-23 12:14:28 +0100

    model done and tested
    Co-authored-by: Kit Trowbridge <kittrow.dev@gmail.com>

--- api/models/userProfile.js
+++ api/models/userProfile.js
@@ -1,6 +1,17 @@
 const mongoose = require("mongoose")
 
 const UserProfileSchema = new mongoose.Schema({
+    authUserId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    favouriteArtists: { type: [String], default: [] },
+    homeLocation: {
+        city: { type: String, default: "Manchester" },
+        lat: { type: Number, default: 53.483959},
+        long: { type: Number, default: -2.244644},
+    }
 })
 
 const UserProfile = mongoose.model("UserProfile", UserProfileSchema)
