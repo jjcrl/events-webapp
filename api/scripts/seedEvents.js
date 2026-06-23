fa262b8c8c6c60820444841521aeccec10746656
jjcrl <j412cff@icloud.com>
2026-06-23 09:47:57 +0100

    basic api fetch

---
+++ api/scripts/seedEvents.js
@@ -0,0 +1,15 @@
+// api/scripts/seedEvents.js
+require('dotenv').config()
+
+const seedEvents = async () => {
+  const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TICKETMASTER_API_KEY}&city=Manchester&countryCode=GB&size=5`
+  
+  console.log("URL:", url)
+  
+  const response = await fetch(url)
+  const data = await response.json()
+  
+  console.log("Response:", JSON.stringify(data, null, 2))
+}
+
+seedEvents().catch(console.error)
