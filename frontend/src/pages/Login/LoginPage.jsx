42ffc805c9f92e41fbea93469e0190ca34291915
jjcrl <j412cff@icloud.com>
2026-06-24 12:31:48 +0100

    conditional render log in or sign up

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -6,10 +6,11 @@ export function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState(null)
-  
+  const [toggle, setToggle] = useState(false)
+  const [name, setName] = useState('')
   const navigate = useNavigate()
 
-  const handleSubmit = async (e) => {
+  const handleLogInSubmit = async (e) => {
     e.preventDefault()
     setError(null)
 
@@ -26,25 +27,77 @@ export function LoginPage() {
     navigate('/feed')
   }
 
+  const handleSignUpSubmit = async (e) => {
+    e.preventDefault()
+    setError(null)
+
+    const { data, error } = await authClient.signUp.email({
+      email,
+      password,
+      name
+    })
+
+    if (error) {
+      setError(error.message)
+      return
+    }
+
+    navigate('/feed')
+  }
+
   return (
     <div>
-      <h1>Log in</h1>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Log in</button>
-      </form>
+
+      {!toggle ? (
+        <>
+          <h1>Log in</h1>
+          {error && <p>{error}</p>}
+          <form onSubmit={handleLogInSubmit}>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Log in</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h1>Sign up</h1>
+          {error && <p>{error}</p>}
+          <form onSubmit={handleSignUpSubmit}>
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Sign up</button>
+          </form>
+
+
+        </>
+      )}
+      <button onClick={(() => { setToggle(!toggle) })}>{toggle ? "login" : "signup"}</button>
     </div>
   )
 }
