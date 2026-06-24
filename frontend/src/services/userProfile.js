39824ff950166c9863aee6ade815d72fc6c9d284
kit-trowbridge <kittrow.dev@gmail.com>
2026-06-24 14:59:03 +0100

    test: all getMyProfile tests passing

--- frontend/src/services/userProfile.js
+++ frontend/src/services/userProfile.js
@@ -2,13 +2,13 @@ const BACKEND_URL = import.meta.env.VITE_API_URL;
 export async function getMyProfile() {
     const requestOptions = {
         method: "GET",
-        credentials: 'include' 
+        credentials: "include"
     }
 
     const response = await fetch(`${BACKEND_URL}/me`, requestOptions)
 
     if (response.status !== 200) {
-        throw new Error("Unable to fetch user");
+        throw new Error("Unable to fetch user's profile");
     }
 
     const data = await response.json();
--- frontend/tests/services/userProfile.test.js
+++ frontend/tests/services/userProfile.test.js
@@ -19,5 +19,36 @@ describe("userProfile services", () => {
             expect(options.method).toEqual("GET");
             expect(options.credentials).toEqual("include");
         });
+        test("returns user profile data on 200", async () => {
+            const mockResponseBody = {
+                profile: { 
+                    authUserId: "abc", 
+                    favouriteArtists: [], 
+                    homeLocation: {
+                        city: "Manchester",
+                        lat: 53.483959,
+                        long: -2.244644
+                    } 
+                }
+                
+            }
+            fetch.mockResponseOnce(
+                JSON.stringify( mockResponseBody ),
+                { status: 200 },
+            );
+
+            const profileData = await getMyProfile();
+
+            expect(profileData).toEqual(mockResponseBody)
+            
+        })
+        test("throws error when status not 200", async () => {
+            fetch.mockResponseOnce(
+                JSON.stringify({ profile: { _id: "abc" } }),
+                { status: 404 },
+            );
+
+            await expect(getMyProfile()).rejects.toThrow("Unable to fetch user's profile");
+        })
     });
 });
