2d08576f86d9d3554bb14fc894863c8c88256b92
jjcrl <j412cff@icloud.com>
2026-07-01 19:54:58 +0100

    addded reset

--- frontend/src/components/EventFilters.jsx
+++ frontend/src/components/EventFilters.jsx
@@ -2,16 +2,24 @@ import { Button } from "./ui/button";
 import CityCombobox from "./CityCombobox";
 import DateRangeFilter from "./DateRangeFilter";
 
+
+
 export default function EventFilters({
   city, from, to, tag,
   cities, topTags,
   onChange,
 }) {
+  const handleClear = () => {
+    onChange({ from: "", to: "", tag: "" })
+  }
+
   return (
+
     <div className="flex flex-col gap-3">
       <section className="flex flex-row gap-3 pb-2 pl-3">
         <CityCombobox city={city} cities={cities} onChange={onChange} />
         <DateRangeFilter from={from} to={to} onChange={onChange} />
+        <Button type="button" variant="outline" className="w-fit px-6 py-6" size="icon" onClick={handleClear}>Reset Filters</Button>
       </section>
       <section>
         {topTags.filter((t) => t !== "Undefined" && t !== "Other").map((tagName) => (
